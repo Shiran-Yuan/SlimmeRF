ef7f2010b37d072da8ddb5c2d49212ac1d460cee
Shiran Yuan <shiran.yuan@dukekunshan.edu.cn>
2024-03-20 20:52:52 +0100

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -1,7 +1,7 @@
 import torch,os,imageio,sys
 from tqdm.auto import tqdm
 from dataLoader.ray_utils import get_rays
-from models.tensoRF import TensorVM, TensorCP, raw2alpha, TensorVMSplit, AlphaGridMask
+from models.tensoRF import TensorCP, raw2alpha, TensorVMSplit, AlphaGridMask
 from utils import *
 from dataLoader.ray_utils import ndc_rays_blender
 
